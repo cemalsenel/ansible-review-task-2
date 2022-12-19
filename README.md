2ccf7fa5950fefa250dd225822ae89352ab0f9d6
cemalsenel <senelcemal61@gmail.com>
2022-12-19 09:10:48 +0100

    add ansible review task 2

---
+++ README.md
@@ -0,0 +1,1 @@
+Ansible review task - 2
---
+++ get_vault_pass.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+
+def get_secret():
+    # complex process to retive secrets...
+    secret='vaulty_pass'
+    print(secret, end='')
+
+get_secret()
